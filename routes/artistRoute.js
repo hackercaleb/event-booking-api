92fa62cfb07920aeb5eaa066056a5c85fb417c1b
John Oshalusi <john.oshalusi@iohk.io>
2023-10-14 12:32:04 +0100

    clean up

--- routes/artistRoute.js
+++ routes/artistRoute.js
@@ -21,42 +21,9 @@ router.put('/updatePassword', protect, updatePassword);
 
 // Artist routes
 router.get('/artists', getAllArtist);
+router.get('/artist/me', protect, getArtistProfile); // Assuming a route for fetching the profile of the currently logged-in artist
 router.get('/artist/:id', getSingleArtist);
-router.get('/artistme', protect, getArtistProfile); // Assuming a route for fetching the profile of the currently logged-in artist
 router.put('/artist', protect, updateArtist);
 router.delete('/artist/', protect, deleteArtist);
 
 module.exports = router;
-
-/*const { Router } = require('express');
-
-const {
-  getAllArtist,
-  getSingleArtist,
-  getArtistProfile,
-  updateArtist,
-  deleteArtist
-} = require('../controllers/artistcontroller');
-
-const { signup, login, updatePassword } = require('../controllers/authcontroller');
-
-const { arrayValidatorRules } = require('../middleware/arrayValidator');
-const { validate } = require('../middleware/validate');
-
-const router = Router();
-
-router.post('/artist', arrayValidatorRules, validate, signup);
-
-router.post('/artist/login', login);
-
-router.get('/artists', getAllArtist);
-
-router.get('/artist/:id', getSingleArtist);
-
-router.get();
-router.put();
-router.put('/updatePassword', protect, updatePassword);
-router.delete();
-
-module.exports = router;
-*/
--- server.js
+++ server.js
@@ -13,22 +13,14 @@ const PORT = 3009;
 
 dotenv.config();
 
-const username = process.env.MONGO_USERNAME;
-const password = process.env.MONGO_PASSWORD;
-const clusterName = process.env.MONGO_CLUSTER_NAME;
-
-const url = `mongodb+srv://${username}:${password}@${clusterName}.dqdxx7i.mongodb.net/?retryWrites=true&w=majority`;
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(url, { useNewUrlParser: true });
+    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
     console.log('MongoDB Atlas Connected');
   } catch (error) {
     console.error('Error connecting to MongoDB Atlas:', error);
   }
 };
-
-//module.exports = connectDB;
 
 connectDB();
 
