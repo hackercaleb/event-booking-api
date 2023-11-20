cfbe763e342fba1ed0c2311866ae49fe901b9f23
hackercaleb <130366225+hackercaleb@users.noreply.github.com>
2023-11-20 11:29:42 +0100

    new bramnh

--- model/bookingmodel.js
+++ model/bookingmodel.js
@@ -9,10 +9,14 @@ const bookingSchema = new mongoose.Schema({
   event: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Event',
-    required: true
+    required: [true, 'Event Id is required']
   },
   booked_at: {
     type: Date,
     default: Date.now
   }
 });
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
