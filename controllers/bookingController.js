e374635c1e627c5305a086a17b50ccf80cc06176
hackercaleb <130366225+hackercaleb@users.noreply.github.com>
2023-11-21 20:43:05 +0100

    worked on the get all bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Event = require('../model/eventmodel');
 const Booking = require('../model/bookingmodel');
 //const { protectUser } = require('./authcontroller');
@@ -6,7 +7,7 @@ const sendEmail = require('../utils/sendEmail');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/apperror');
 
-const createBooking = catchAsync(async (req, res, next) => {
+exports.createBooking = catchAsync(async (req, res, next) => {
   //const { id } = req.user;
   const { eventId } = req.body;
 
@@ -60,4 +61,62 @@ const createBooking = catchAsync(async (req, res, next) => {
   });
 });
 
-module.exports = createBooking;
+exports.getBookings = catchAsync(async (req, res, next) => {
+  try {
+    // filtering by any field in the database
+    // build query
+    const queryObj = { ...req.query };
+    const excludedField = ['limit', 'sort', 'filter'];
+    excludedField.forEach((el) => delete queryObj[el]);
+    console.log(req.query, queryObj);
+
+    let query = Booking.find(queryObj);
+
+    // validate filter parameter
+    if (req.query.filter) {
+      if (
+        typeof req.query.filter !== 'string' ||
+        !mongoose.Types.ObjectId.isValid(req.query.filter)
+      ) {
+        return next(new AppError('Filter must be a valid ObjectId', 400));
+      }
+      query = query.where('event').equals(req.query.filter);
+    }
+
+    // validate sort parameter
+    if (req.query.sort) {
+      if (typeof req.query.sort !== 'string') {
+        return next(new AppError('Sort must be a string', 400));
+      }
+      query = query.sort(req.query.sort);
+    }
+
+    // validate limit parameter
+    if (req.query.limit) {
+      const limit = parseInt(req.query.limit, 10);
+      // eslint-disable-next-line no-restricted-globals
+      if (isNaN(limit)) {
+        return next(new AppError('Limit must be a number', 400));
+      }
+      query = query.limit(limit);
+    }
+
+    // execute query
+    const bookings = await query;
+
+    // send response
+    return res.status(200).json({
+      status: 'success',
+      results: bookings.length,
+      data: {
+        bookings
+      }
+    });
+  } catch (error) {
+    console.error('error', error);
+    return res.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error'
+    });
+  }
+});
--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 
-const createBooking = require('../controllers/bookingController');
+const { createBooking, getBookings } = require('../controllers/bookingController');
 
 const { protectUser } = require('../controllers/authcontroller');
 
 const router = Router();
 
 router.post('/bookings', protectUser, createBooking);
+router.get('/bookings', getBookings);
 
 module.exports = router;
